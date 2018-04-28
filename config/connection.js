3cdcb10c50814af0b66664ff1c3c9b1f819ab3cd
Nicolas Brent Ellerbe <31285115+Nbrente@users.noreply.github.com>
2018-04-28 15:04:19 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "tpoH1004",
+  password: "",
   database: "burger_db"
 });
 
