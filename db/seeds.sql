af1a18e83dee4067d2ad4af168652cb24b8c5e4c
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-04-28 15:24:18 -0400

    added seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 0);
+INSERT INTO burger
+  (name, isEaten)
+VALUES
+  ('Bacon Cheese Burger', 1);
